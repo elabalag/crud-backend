7b7c3685e75873856451db7a9b8adfc5cfcf4b0a
elabalag <elamurugan.balachandiran@gmail.com>
2025-05-28 00:15:17 +0530

    Your commit message

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ app.use("/tasks", taskRoutes);
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI;
+
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => {
     console.error("❌ DB connection failed:", err.message);
-    process.exit(1);
+    process.exit(1); // exit the app if DB connection fails
   });
 
 // Server start
